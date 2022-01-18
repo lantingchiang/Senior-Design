fc3c90d68c8dcee1d1111fff91ca5a4b7dd1b591
Lanting Chiang <lantingchiang@gmail.com>
2022-01-18 16:50:44 -0500

    comment out WIP queries for raw materials

--- commercial-paper/organization/material-prov/contract/lib/rawmaterialcontract.js
+++ commercial-paper/organization/material-prov/contract/lib/rawmaterialcontract.js
@@ -148,91 +148,91 @@ class RawMaterialContract extends Contract {
 
     // Query transactions
 
-    /**
-     * Query history of a raw material
-     * @param {Context} ctx the transaction context
-     * @param {String} manufacturer of raw material
-     * @param {String} batchNumber of raw material
-    */
-    async queryHistory(ctx, batchNumber, manufacturer) {
-        let query = new RawMaterialQueryUtils(ctx, 'org.vaccine.rawmaterial');
-        // pass in primary key composed of batch number and manufacturer
-        let results = await query.getAssetHistory(batchNumber, manufacturer);
-        return results;
-    }
+    // /**
+    //  * Query history of a raw material
+    //  * @param {Context} ctx the transaction context
+    //  * @param {String} manufacturer of raw material
+    //  * @param {String} batchNumber of raw material
+    // */
+    // async queryHistory(ctx, batchNumber, manufacturer) {
+    //     let query = new RawMaterialQueryUtils(ctx, 'org.vaccine.rawmaterial');
+    //     // pass in primary key composed of batch number and manufacturer
+    //     let results = await query.getAssetHistory(batchNumber, manufacturer);
+    //     return results;
+    // }
 
-    /**
-    * queryOwner commercial paper: supply name of owning org, to find list of papers based on owner field
-    * @param {Context} ctx the transaction context
-    * @param {String} owner commercial paper owner
-    */
-    async queryOwner(ctx, owner) {
+    // /**
+    // * queryPurchaser raw material: supply name of purchaser to find list of raw materials this purchaser purchased
+    // * @param {Context} ctx the transaction context
+    // * @param {String} owner commercial paper owner
+    // */
+    // async queryPurchaser(ctx, owner) {
 
-        let query = new QueryUtils(ctx, 'org.papernet.paper');
-        let owner_results = await query.queryKeyByOwner(owner);
+    //     let query = new QueryUtils(ctx, 'org.papernet.paper');
+    //     let owner_results = await query.queryKeyByOwner(owner);
 
-        return owner_results;
-    }
+    //     return owner_results;
+    // }
 
-    /**
-    * queryPartial commercial paper - provide a prefix eg. "DigiBank" will list all papers _issued_ by DigiBank etc etc
-    * @param {Context} ctx the transaction context
-    * @param {String} prefix asset class prefix (added to paperlist namespace) eg. org.papernet.paperMagnetoCorp asset listing: papers issued by MagnetoCorp.
-    */
-    async queryPartial(ctx, prefix) {
+    // /**
+    // * queryPartial commercial paper - provide a prefix eg. "DigiBank" will list all papers _issued_ by DigiBank etc etc
+    // * @param {Context} ctx the transaction context
+    // * @param {String} prefix asset class prefix (added to paperlist namespace) eg. org.papernet.paperMagnetoCorp asset listing: papers issued by MagnetoCorp.
+    // */
+    // async queryPartial(ctx, prefix) {
 
-        let query = new QueryUtils(ctx, 'org.papernet.paper');
-        let partial_results = await query.queryKeyByPartial(prefix);
+    //     let query = new QueryUtils(ctx, 'org.papernet.paper');
+    //     let partial_results = await query.queryKeyByPartial(prefix);
 
-        return partial_results;
-    }
+    //     return partial_results;
+    // }
 
-    /**
-    * queryAdHoc commercial paper - supply a custom mango query
-    * eg - as supplied as a param:     
-    * ex1:  ["{\"selector\":{\"faceValue\":{\"$lt\":8000000}}}"]
-    * ex2:  ["{\"selector\":{\"faceValue\":{\"$gt\":4999999}}}"]
-    * 
-    * @param {Context} ctx the transaction context
-    * @param {String} queryString querystring
-    */
-    async queryAdhoc(ctx, queryString) {
+    // /**
+    // * queryAdHoc commercial paper - supply a custom mango query
+    // * eg - as supplied as a param:     
+    // * ex1:  ["{\"selector\":{\"faceValue\":{\"$lt\":8000000}}}"]
+    // * ex2:  ["{\"selector\":{\"faceValue\":{\"$gt\":4999999}}}"]
+    // * 
+    // * @param {Context} ctx the transaction context
+    // * @param {String} queryString querystring
+    // */
+    // async queryAdhoc(ctx, queryString) {
 
-        let query = new QueryUtils(ctx, 'org.papernet.paper');
-        let querySelector = JSON.parse(queryString);
-        let adhoc_results = await query.queryByAdhoc(querySelector);
+    //     let query = new QueryUtils(ctx, 'org.papernet.paper');
+    //     let querySelector = JSON.parse(queryString);
+    //     let adhoc_results = await query.queryByAdhoc(querySelector);
 
-        return adhoc_results;
-    }
+    //     return adhoc_results;
+    // }
 
 
-    /**
-     * queryNamed - supply named query - 'case' statement chooses selector to build (pre-canned for demo purposes)
-     * @param {Context} ctx the transaction context
-     * @param {String} queryname the 'named' query (built here) - or - the adHoc query string, provided as a parameter
-     */
-    async queryNamed(ctx, queryname) {
-        let querySelector = {};
-        switch (queryname) {
-            case "redeemed":
-                querySelector = { "selector": { "currentState": 4 } };  // 4 = redeemd state
-                break;
-            case "trading":
-                querySelector = { "selector": { "currentState": 3 } };  // 3 = trading state
-                break;
-            case "value":
-                // may change to provide as a param - fixed value for now in this sample
-                querySelector = { "selector": { "faceValue": { "$gt": 4000000 } } };  // to test, issue CommPapers with faceValue <= or => this figure.
-                break;
-            default: // else, unknown named query
-                throw new Error('invalid named query supplied: ' + queryname + '- please try again ');
-        }
+    // /**
+    //  * queryNamed - supply named query - 'case' statement chooses selector to build (pre-canned for demo purposes)
+    //  * @param {Context} ctx the transaction context
+    //  * @param {String} queryname the 'named' query (built here) - or - the adHoc query string, provided as a parameter
+    //  */
+    // async queryNamed(ctx, queryname) {
+    //     let querySelector = {};
+    //     switch (queryname) {
+    //         case "redeemed":
+    //             querySelector = { "selector": { "currentState": 4 } };  // 4 = redeemd state
+    //             break;
+    //         case "trading":
+    //             querySelector = { "selector": { "currentState": 3 } };  // 3 = trading state
+    //             break;
+    //         case "value":
+    //             // may change to provide as a param - fixed value for now in this sample
+    //             querySelector = { "selector": { "faceValue": { "$gt": 4000000 } } };  // to test, issue CommPapers with faceValue <= or => this figure.
+    //             break;
+    //         default: // else, unknown named query
+    //             throw new Error('invalid named query supplied: ' + queryname + '- please try again ');
+    //     }
 
-        let query = new QueryUtils(ctx, 'org.papernet.paper');
-        let adhoc_results = await query.queryByAdhoc(querySelector);
+    //     let query = new QueryUtils(ctx, 'org.papernet.paper');
+    //     let adhoc_results = await query.queryByAdhoc(querySelector);
 
-        return adhoc_results;
-    }
+    //     return adhoc_results;
+    // }
 
 }
 
