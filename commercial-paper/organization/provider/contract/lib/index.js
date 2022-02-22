a4b34956af551c375b6f8e5252b463f202960599
emilysap13 <emily.saperstein@gmail.com>
2022-02-21 23:27:09 -0500

    Added lib/package.json, lib/index.js, cli doctor files for individual and provider orgs.

---
+++ commercial-paper/organization/provider/contract/lib/index.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+
+const vaccinecontract = require('./lib/vaccinecontract.js');
+module.exports.contracts = [vaccinecontract];
